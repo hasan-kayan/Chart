d8258256c48b3faca3b20d1b339d04100a56d999
hasan-kayan <hasankayan2000@hotmail.com>
2025-09-30 22:04:41 +0300

    Cleared for a task

--- charts/exporter.py
+++
@@ -1,7 +0,0 @@
-# excel_chart_ui/charts/exporter.py
-
-def export_chart(fig, path):
-    if path.endswith(".html"):
-        fig.write_html(path)
-    else:
-        fig.write_image(path, engine="kaleido")
